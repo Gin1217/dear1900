fa4acb77e1945a7c4e5c95164ab0d12123ca21e0
安懿 <gin@anyideMacBook-Air.local>
2021-01-30 23:05:06 +0800

    我的音乐能听啦！！

--- miniprogram/pages/player/player.js
+++ miniprogram/pages/player/player.js
@@ -1,9 +1,8 @@
 // pages/player/player.js
-let musiclist =[]
+let musiclist = []
 //正在播放的歌曲的Index
-let playingIndex=0
-
-
+let playingIndex = 9
+const backgroundAudioManager=wx.getBackgroundAudioManager()
 
 Page({
 
@@ -11,7 +10,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    picUrl:''
+    picUrl: '',
+    isPlaying: false
   },
 
   /**
@@ -19,29 +19,74 @@ Page({
    */
   onLoad: function (options) {
     console.log(options)
-    console.log(options.musicId, typeof(options.musicId))
-    playingIndex=options.index
-    musiclist=wx.getStorageSync('musiclist')
+    console.log(options.musicId, typeof (options.musicId))
+    playingIndex = options.index
+    musiclist = wx.getStorageSync('musiclist')
     this._loadMusicDetail(options.musicId)
   },
 
-  _loadMusicDetail(musicId){
-    let music=musiclist[playingIndex]
+  _loadMusicDetail(musicId) {
+    let music = musiclist[playingIndex]
     console.log(music)
     wx.setNavigationBarTitle({
-      title:music.name,
+      title: music.name,
     })
     this.setData({
-      picUrl:music.al.picUrl
+      picUrl: music.al.picUrl
     })
     wx.cloud.callFunction({
-      name:'music',
-      data:{
+      name: 'music',
+      data: {
         musicId,
-        $url:'musicUrl'
+        $url: 'musicUrl'
       }
-    }).then((res)=>{
+    }).then((res) => {
       console.log(res)
+      const url = res.result.data[0].url
+      if (url === null) {
+        wx.showToast({
+          title:'没有权限播放'
+        })
+        backgroundAudioManager.pause()
+        this.setData({
+          isPlaying:false
+        })
+        return
+      }
+      backgroundAudioManager.src=url
+      backgroundAudioManager.title=music.name
+      backgroundAudioManager.coverImgUrl=music.al.picUrl
+      backgroundAudioManager.singer=music.ar[0].name
+
+      this.setData({
+        isPlaying:true
+      })
+      wx.hideLoading()
     })
+  },
+
+  togglePlaying(){
+    if(this.data.isPlaying){
+      backgroundAudioManager.pause()
+    }else{
+      backgroundAudioManager.play()
+    }
+    this.setData({
+      isPlaying: !this.data.isPlaying
+    })
+  },
+  onPrev(){
+    playingIndex--
+    if(playingIndex === 0){
+      playingIndex = musiclist.length -1
+    }
+      this._loadMusicDetail(musiclist[playingIndex].id)
+  },
+  onNext(){
+    playingIndex++
+    if(playingIndex === musiclist.length){
+      playingIndex = 0
+    }
+      this._loadMusicDetail(musiclist[playingIndex].id)
   }
 })
